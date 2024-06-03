b1bc4d121702efb689fdb1d7e4c9c2d76537a62f
fathurrahmanhfz <fathurrahmanhafiz552@gmail.com>
2024-06-03 08:47:51 +0700

    firsst

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+const {
+    addBookHandler,
+    getAllBookHandler,
+    getBookByIdHandler,
+    editBookByIdHandler,
+    deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: () => {
+            return 'Homepage Bookshelf API Dicoding Fathurrahman Hafiz'
+        },
+    },
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: getBookByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: editBookByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBookByIdHandler,
+    },
+]
+
+module.exports = routes;
